e01f0fc044b53f91ba3a1325d83f7c3d80feb486
Iurii Rudov <iuriirudov@gmail.com>
2022-08-11 23:43:59 -0400

    Initial commit of the example working with MongoDB local storage.

---
+++ src/main/java/com/example/javamongodb/Customer.java
@@ -0,0 +1,46 @@
+package com.example.javamongodb;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ * Customer
+ */
+public class Customer {
+
+    @Id
+    public String id;
+
+    public String firstName;
+    public String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [firstName=" + firstName + ", id=" + id + ", lastName=" + lastName + "]";
+    }
+
+}
